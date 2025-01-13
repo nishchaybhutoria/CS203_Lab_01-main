53acb9866298d5b5111333a6abe57160a0d43002
Nishchay Bhutoria <bhutorianishchay@gmail.com>
2025-01-13 14:07:48 +0530

    Set up Jaeger

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ import logging
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.trace import Status, StatusCode
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -42,8 +43,8 @@ FlaskInstrumentor().instrument_app(app)
 
 # Configure OpenTelemetry
 trace.set_tracer_provider(TracerProvider())
-console_exporter = ConsoleSpanExporter()
-span_processor = BatchSpanProcessor(console_exporter)
+jaeger_exporter = JaegerExporter(agent_host_name="localhost", agent_port=6831)
+span_processor = BatchSpanProcessor(jaeger_exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
 
 # Utility Functions
